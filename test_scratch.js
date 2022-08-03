600f8bfdf8cee8ca6057f8d025369e60da66ab9a
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-08-03 18:30:35 -0500

    Update test_scratch.js
    added raise unit tests

--- test_scratch.js
+++ test_scratch.js
@@ -7,29 +7,50 @@ poker.funcDefs.forEach(d => {
 
 const replyFunc = s => console.log(s);
 
+const argMap = new Map();
+
 const testUser = {
     'id': 0,
     'username': 'testUser',
     'tag': 'testUser#0'
 }
 
+const testUser2 = {
+    'id': 1,
+    'username': 'testUser2',
+    'tag': 'testUser2#0'
+}
+
 const testContext = {
     'guildId': 0,
     'reply': replyFunc,
-    'user': testUser
+    'user': testUser,
+    'options': {
+        'getInteger': s => argMap.get(s)
+    }
+}
+
+const generateNewContext = (user) => {
+    return {
+        'guildId': 0,
+        'reply': replyFunc,
+        'user': user,
+        'options': {
+            'getInteger': s => argMap.get(s)
+        }
+    };
 }
 
 poker_funcs.get('poker_new')(testContext);
+
 poker_funcs.get('poker_join')(testContext);
+poker_funcs.get('poker_join')(generateNewContext(testUser2));
+
 poker_funcs.get('poker_start')(testContext);
+
 poker_funcs.get('poker_call')(testContext);
-poker_funcs.get('poker_raise')(testContext);
+
+argMap.set('amount', 600);
+poker_funcs.get('poker_raise')(generateNewContext(testUser2));
+
 poker_funcs.get('poker_call')(testContext);
-poker_funcs.get('poker_call')(testContext);
-poker_funcs.get('poker_balance')(testContext);
-poker_funcs.get('poker_start')(testContext);
-poker_funcs.get('poker_call')(testContext);
-poker_funcs.get('poker_call')(testContext);
-poker_funcs.get('poker_call')(testContext);
-poker_funcs.get('poker_call')(testContext);
-poker_funcs.get('poker_balance')(testContext);
